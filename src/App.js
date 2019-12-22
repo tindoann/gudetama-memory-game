2c537dd662db300442ded212c316c4f975251e04
tindoann <tindoann@gamil.com>
2019-12-22 17:34:56 -0500

    working img card

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react' 
+import Card from './components/Cards'
 
-function App() {
+export default function App() {
+  const [flipped, setFlipped] = useState([])
+  const handleClick = id => setFlipped([...flipped, id])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Can you remember where the cards are?</h2>
+      <Card 
+        id={1}
+        width={100}
+        height={100}
+        back={'../img/gut1.png'}
+        front={'../img/gut2.png'}
+        flipped={flipped.includes(1)}
+        handleClick={() => handleClick(1)}
+        />
     </div>
-  );
+  )
 }
 
-export default App;
