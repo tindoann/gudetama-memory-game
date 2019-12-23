59f194b6fe58de1c7cb8ccf1a93d8069638326e5
tindoann <tindoann@gamil.com>
2019-12-22 20:03:26 -0500

    added deck and shuffle

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React, { useState } from 'react' 
+import React, { useState, useEffect } from 'react' 
 import Card from './components/Cards'
+import Board from './component/board'
+import initializeDeck from './deck'
 
 export default function App() {
+  const [cards, setCards] = useState([])
   const [flipped, setFlipped] = useState([])
+  const [dimension, setDimension] = useState(400)
+  
+  useEffect(() => {
+    resizeBoard()
+    setCards(initializeDeck())
+  }, [])
+  
   const handleClick = id => setFlipped([...flipped, id])
 
+  const resizeBoard = () => {
+    setDimension(
+      Math.min(document.documentElement.clientWidth, document.documentElement.clientHeight,
+      ),
+    )
+  }
+  
   return (
     <div>
       <h2>Can you remember where the cards are?</h2>
-      <Card 
-        id={1}
-        width={100}
-        height={100}
-        back={'../img/gut1.png'}
-        front={'../img/gut2.png'}
-        flipped={flipped.includes(1)}
-        handleClick={() => handleClick(1)}
-        />
+      <Board cards={cards} flipped={flipped} handleClick={handleClick} />
     </div>
   )
 }
--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -26,7 +26,7 @@ export default function Card({
                     height, width
                 }}
                 className={flipped ? 'front' : 'back'}
-                src={flipped || solved ? `/img/${type}.png` : '/img/gut1.png' }
+                src={flipped || solved ? `/img/${curr}.png` : '/img/gut1.png' }
                 alt={type}
             />
         </div>
